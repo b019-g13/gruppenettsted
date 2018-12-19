c02535add91e3f4ea6251831e8f99c7cbd178c2c
Bjørnar Hagen <bjornar@datahjelpen.no>
2018-12-19 03:09:06 +0100

    Added fonts. Some basic styling

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,5 @@
 {{-- Scripts --}}
 <script src="{{ asset('js/app.js') }}" defer></script>
 {{-- Fonts --}}
-<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://use.typekit.net/agc8mck.css">
+<link rel="stylesheet" href="https://cdn.datahjelpen.no/fonts/butler/butler-700.css">
