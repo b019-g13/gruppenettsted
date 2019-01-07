1c87e7b9453ca4752a0ee6ec4425c0a45b87fa69
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-01-07 17:27:59 +0100

    Updated navigation links for logged-in users

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -10,13 +10,16 @@
             @if (Route::has('register'))
                 <a href="{{ route('register') }}">{{ __('Register') }}</a>
             @endif
-        @else
-            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        @endguest
+        @auth
+            <a href="{{ route('dashboard') }}">Kontrollpanel</a>
+            <a href="{{ route('post.index.admin') }}">Innlegg</a>
+            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                 {{ __('Logout') }}
             </a>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
             </form>
-        @endguest
+        @endauth
     </div>
 </nav>
