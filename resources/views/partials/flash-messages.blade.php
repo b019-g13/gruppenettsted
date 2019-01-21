8feae6a50dbaa27981f123336c0a2f7ee0a9f429
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-01-21 13:01:53 +0100

    Styled flash messages and made the dismissible

--- resources/views/partials/flash-messages.blade.php
+++ resources/views/partials/flash-messages.blade.php
@@ -1,13 +1,25 @@
 <ul id="flash-message">
     @foreach (['error', 'warning', 'success', 'info'] as $msg)
         @if (Session::has($msg))
-            <li class="alert-{{ $msg }}">{!! Session::get($msg) !!}</li>
+            <li class="alert-{{ $msg }}">
+                @php
+                    $random_id = 'flash-message-' . md5(uniqid(rand(), true));
+                @endphp
+                <input type="checkbox" id="{{$random_id}}">
+                <label for="{{$random_id}}">{!! Session::get($msg) !!}</label>
+            </li>
         @endif
     @endforeach
 
     @if ($errors->any())
         @foreach ($errors->all() as $error)
-            <li class="alert-error">{{ $error }}</li>
+            <li class="alert-error">
+                @php
+                    $random_id = 'flash-message-' . md5(uniqid(rand(), true));
+                @endphp
+                <input type="checkbox" id="{{$random_id}}">
+                <label for="{{$random_id}}">{{ $error }}</label>
+            </li>
         @endforeach
     @endif
 </ul>
