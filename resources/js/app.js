73487f62a6e261d1034a249cc6e4c4f68a12d0e2
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-01-21 13:27:06 +0100

    Fixed markdown not working on all pages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,51 +1,61 @@
-const article = document.querySelector("main article");
+const articles = document.querySelectorAll("article");
 let articleContent = false;
 let isAdminPage = false;
+let isPostPage = false;
 
-if (article) {
-  articleContent = article.querySelector(".content");
-}
-
-// Check if we are on the edit page
+// Check if we are on an admin post page
 if (
-    document.body.classList.contains("posts-create") ||
-    document.body.classList.contains("posts-edit")
+  document.body.classList.contains("posts-create") ||
+  document.body.classList.contains("posts-edit")
 ) {
-    // posts-create
-    isAdminPage = true;
-    articleContent = document.querySelector("textarea#post_content");
+  // posts-create
+  isAdminPage = true;
 }
 
+// Check if we are on a regular post page
+if (
+  document.body.classList.contains("posts-index") ||
+  document.body.classList.contains("posts-show")
+) {
+  // posts-create
+  isPostPage = true;
+}
 
-if (articleContent) {
-  const showdown  = require("showdown");
-  const converter = new showdown.Converter();
-  let articleTitle;
-  let articlePreview;
-  let articlePreviewTitle;
+for (let i = 0; i < articles.length; i++) {
+  let article = articles[i];
+  articleContent = article.querySelector(".content");
 
-  if (isAdminPage) {
-    articleTitle = document.querySelector("#post_title");
-    articlePreview = document.querySelector("#post_content-preview");
-    articlePreviewTitle = article.querySelector(".title");
+  if (articleContent) {
+    const showdown  = require("showdown");
+    const converter = new showdown.Converter();
+    let articleTitle;
+    let articlePreview;
+    let articlePreviewTitle;
 
-    // Render markdown as HTML
-    articleTitle.addEventListener("keyup", adminPreviewRender);
-    articleContent.addEventListener("keyup", adminPreviewRender);
-    adminPreviewRender();
-  } else if (document.body.classList.contains("posts-show")) {
-    // Render markdown as HTML
-    articleContent.innerHTML = converter.makeHtml(articleContent.innerHTML);
+    if (isAdminPage) {
+      articleTitle = document.querySelector("#post_title");
+      articleContent = document.querySelector("textarea#post_content");
+      articlePreview = document.querySelector("#post_content-preview");
+      articlePreviewTitle = article.querySelector(".title");
 
-    // Remove image if it's just the placeholder
-    let articleImg = article.querySelector("img:first-child");
-    if (articleImg && articleImg.getAttribute("src") == "/storage/placeholder-wide.png") {
-      article.removeChild(articleImg);
+      // Render markdown as HTML
+      articleTitle.addEventListener("keyup", adminPreviewRender);
+      articleContent.addEventListener("keyup", adminPreviewRender);
+      adminPreviewRender();
+    } else if (isPostPage) {
+      // Render markdown as HTML
+      articleContent.innerHTML = converter.makeHtml(articleContent.innerHTML);
+
+      // Remove image if it's just the placeholder
+      let articleImg = article.querySelector("img:first-child");
+      if (articleImg && articleImg.getAttribute("src") == "/storage/placeholder-wide.png") {
+        article.removeChild(articleImg);
+      }
     }
-  }
 
-  function adminPreviewRender() {
-    articlePreview.innerHTML = converter.makeHtml(articleContent.value);
-    articlePreviewTitle.innerHTML = articleTitle.value;
+    function adminPreviewRender() {
+      articlePreview.innerHTML = converter.makeHtml(articleContent.value);
+      articlePreviewTitle.innerHTML = articleTitle.value;
+    }
   }
 }
